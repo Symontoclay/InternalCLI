d16333a1f186d70cffb1661215c4146192b3b0ed
Sergiy Tolkachov <metatypeman@gmail.com>
2020-11-08 11:54:02 +0200

    sprint 7 PageCreator initial commit

---
+++ PageCreator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PageCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
