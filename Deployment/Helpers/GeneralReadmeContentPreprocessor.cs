5c8b0f8d319de6e3c856361383374c21ba0b5647
metatypeman <metatypeman@gmail.com>
2021-08-20 19:38:17 +0300

    DevOps in progress...

--- Deployment/Helpers/GeneralReadmeContentPreprocessor.cs
+++ Deployment/Helpers/GeneralReadmeContentPreprocessor.cs
@@ -3,6 +3,7 @@ using NLog;
 using SiteBuilder.HtmlPreprocessors;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,16 +13,16 @@ namespace Deployment.Helpers
     public static class GeneralReadmeContentPreprocessor
     {
 #if DEBUG
-        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        //private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
 #endif
 
         public static string Run(string initialContent, string commonBadgesFileName, string repositorySpecificBadgesFileName, string repositorySpecificReadmeFileName)
         {
 #if DEBUG
-            _logger.Info($"commonBadgesFileName = {commonBadgesFileName}");
-            _logger.Info($"repositorySpecificBadgesFileName = {repositorySpecificBadgesFileName}");
-            _logger.Info($"repositorySpecificReadmeFileName = {repositorySpecificReadmeFileName}");
-            _logger.Info($"initialContent = '{initialContent}'");
+            //_logger.Info($"commonBadgesFileName = {commonBadgesFileName}");
+            //_logger.Info($"repositorySpecificBadgesFileName = {repositorySpecificBadgesFileName}");
+            //_logger.Info($"repositorySpecificReadmeFileName = {repositorySpecificReadmeFileName}");
+            //_logger.Info($"initialContent = '{initialContent}'");
 #endif
 
             var doc = new HtmlDocument();
@@ -34,15 +35,66 @@ namespace Deployment.Helpers
 
         private static void DiscoverNodes(HtmlNode rootNode, HtmlDocument doc, string commonBadgesFileName, string repositorySpecificBadgesFileName, string repositorySpecificReadmeFileName)
         {
-#if DEBUG
-            if (rootNode.Name != "#document")
+//#if DEBUG
+            //if (rootNode.Name != "#document")
+            //{
+            //    _logger.Info($"rootNode.Name = '{rootNode.Name}'");
+            //    _logger.Info($"rootNode.OuterHtml = {rootNode.OuterHtml}");
+            //    _logger.Info($"rootNode.InnerHtml = {rootNode.InnerHtml}");
+            //    _logger.Info($"rootNode.InnerText = {rootNode.InnerText}");
+            //}
+//#endif
+
+            if(rootNode.Name == "spec_readme")
             {
-                _logger.Info($"rootNode.Name = '{rootNode.Name}'");
-                _logger.Info($"rootNode.OuterHtml = {rootNode.OuterHtml}");
-                _logger.Info($"rootNode.InnerHtml = {rootNode.InnerHtml}");
-                _logger.Info($"rootNode.InnerText = {rootNode.InnerText}");
+                var newNode = doc.CreateElement("div");
+                var parentNode = rootNode.ParentNode;
+
+                parentNode.ReplaceChild(newNode, rootNode);
+
+                if (!string.IsNullOrWhiteSpace(repositorySpecificReadmeFileName))
+                {
+                    var content = File.ReadAllText(repositorySpecificReadmeFileName);
+
+                    newNode.InnerHtml = content;
+                }
+
+                return;
             }
-#endif
+
+            if(rootNode.Name == "gen_badges")
+            {
+                var newNode = doc.CreateElement("span");
+                var parentNode = rootNode.ParentNode;
+
+                parentNode.ReplaceChild(newNode, rootNode);
+
+                if (!string.IsNullOrWhiteSpace(commonBadgesFileName))
+                {
+                    var content = File.ReadAllText(commonBadgesFileName);
+
+                    newNode.InnerHtml = content;
+                }
+
+                return;
+            }
+
+            if (rootNode.Name == "spec_badges")
+            {
+                var newNode = doc.CreateElement("span");
+                var parentNode = rootNode.ParentNode;
+
+                parentNode.ReplaceChild(newNode, rootNode);
+
+                if (!string.IsNullOrWhiteSpace(repositorySpecificBadgesFileName))
+                {
+                    var content = File.ReadAllText(repositorySpecificBadgesFileName);
+
+                    newNode.InnerHtml = content;
+                }
+
+                return;
+            }
 
             foreach (var node in rootNode.ChildNodes.ToList())
             {
