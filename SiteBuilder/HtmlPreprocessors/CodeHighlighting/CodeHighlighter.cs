187f326d3f876eee40fdcd873dfefbe460942cea
metatypeman <metatypeman@gmail.com>
2023-02-24 18:15:55 +0200

    0.4.3 keywords

--- SiteBuilder/HtmlPreprocessors/CodeHighlighting/CodeHighlighter.cs
+++ SiteBuilder/HtmlPreprocessors/CodeHighlighting/CodeHighlighter.cs
@@ -38,7 +38,7 @@ namespace SiteBuilder.HtmlPreprocessors.CodeHighlighting
             "relation", "inh", "rel", "null", "fun", "string", "fuzzy", "number", "error", "try", "catch", "where", "else", "ensure", "action", "actions", 
             "op", "complete", "break", "await", "wait", "alias", "repeat", "while", "continue", "return", "if", "elif", "true", "false", "var", "default", "state",
             "enter", "leave", "states", "as", "private", "protected", "public", "down", "duration", "add", "fact", "reject", "exec", "synonym", "idle", "with",
-            "priority", "import"
+            "priority", "import", "ctor", "new"
         };
 
         private static List<string> _symOntoClayLargeSpaceMarksList = new List<string>()
