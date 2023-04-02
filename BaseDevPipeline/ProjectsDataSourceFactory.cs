23703bf506355096f31d6a92b40c605f53202a03
metatypeman <metatypeman@gmail.com>
2023-04-02 10:50:55 +0300

    snapshot

---
+++ BaseDevPipeline/ProjectsDataSourceFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseDevPipeline
+{
+    public static class ProjectsDataSourceFactory
+    {
+        public static ProjectsDataSourceMode Mode { get; set; } = ProjectsDataSourceMode.Prod;
+    }
+}
