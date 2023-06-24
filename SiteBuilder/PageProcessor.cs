b48d2175769dde80d4e3bfed6364a0b169d0a418
metatypeman <metatypeman@gmail.com>
2023-06-24 13:24:46 +0300

    0.5.2 jquery and bootstrap are hosted on the site

--- SiteBuilder/PageProcessor.cs
+++ SiteBuilder/PageProcessor.cs
@@ -43,10 +43,13 @@ namespace SiteBuilder
 
         private static void SetUpBootstrap()
         {
-            _commonCssLinksList.Add("<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css' integrity='sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2' crossorigin='anonymous'>");
+            //_commonCssLinksList.Add("<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css' integrity='sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2' crossorigin='anonymous'>");
+            _commonCssLinksList.Add("<link rel='stylesheet' href='assets/bootstrap_4.5.3/bootstrap.min.css'>");
 
-            _commonJsLinksList.Add("<script src='https://code.jquery.com/jquery-3.5.1.slim.min.js' integrity='sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj' crossorigin='anonymous'></script>");
-            _commonJsLinksList.Add("<script src='https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js' integrity='sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx' crossorigin='anonymous'></script>");
+            //_commonJsLinksList.Add("<script src='https://code.jquery.com/jquery-3.5.1.slim.min.js' integrity='sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj' crossorigin='anonymous'></script>");
+            _commonJsLinksList.Add("<script src='assets/jquery-3.5.1/jquery-3.5.1.slim.min.js'></script>");
+            //_commonJsLinksList.Add("<script src='https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js' integrity='sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx' crossorigin='anonymous'></script>");
+            _commonJsLinksList.Add("<script src='assets/bootstrap_4.5.3/bootstrap.bundle.min.js'></script>");
         }
 
         private static void SetUpFontAwesome()
