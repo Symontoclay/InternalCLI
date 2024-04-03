dc290e820c50ea10d613e8cd30a3ff85559c0602
metatypeman <metatypeman@gmail.com>
2024-04-03 17:46:30 +0300

    added tests

---
+++ PipelinesTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace PipelinesTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
