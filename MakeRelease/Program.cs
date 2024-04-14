92ddf7adfa6f7960080c1df465ec824c91b39c7b
metatypeman <metatypeman@gmail.com>
2024-04-14 13:58:31 +0300

    Code cleansing

--- MakeRelease/Program.cs
+++ MakeRelease/Program.cs
@@ -73,24 +73,6 @@ namespace MakeRelease
                 default:
                     throw new ArgumentOutOfRangeException(nameof(runMode), runMode, null);
             }
-
-            //Console.WriteLine($"This app will release future version {version}. Are you sure?");
-            //Console.WriteLine("Press 'y' or 'Y' for release or other else key for cancel release.");
-            //Console.WriteLine("After your choise press enter.");
-
-            //var key = Console.ReadLine();
-
-            //if(key != "y" && key != "Y")
-            //{
-            //    Console.WriteLine("Release has been cancelled.");
-
-            //    return;
-            //}
-
-            //Console.WriteLine("Release has been started.");
-
-            //var task = new MakeReleaseReleaseTask();
-            //task.Run();
         }
 
         private static void MakeReleaseOnTest()
