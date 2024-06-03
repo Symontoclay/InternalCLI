79189b421500bf9ef4b7e1f122a0e59d99728870
metatypeman <metatypeman@gmail.com>
2024-06-03 18:20:19 +0300

    0.5.4 snapshot

--- CSharpUtilsTests/CSharpProjectHelperNetTests.cs
+++ CSharpUtilsTests/CSharpProjectHelperNetTests.cs
@@ -4,29 +4,3 @@
     {
     }
 }
-
-/*
-        public static List<string> GetCSharpFileNames(string projectFileName)
-        public static string GetTargetFramework(string projectFileName)
-        public static (KindOfTargetCSharpFramework Kind, Version Version) GetTargetFrameworkVersion(string projectFileName)
-        public static (KindOfTargetCSharpFramework Kind, Version Version) ConvertTargetFrameworkToVersion(string targetFramework)
-        public static string ConvertVersionToTargetFramework((KindOfTargetCSharpFramework Kind, Version Version) frameworkVersion)
-        public static bool SetTargetFramework(string projectFileName, string targetFramework)
-        public static bool SetTargetFramework(string projectFileName, (KindOfTargetCSharpFramework Kind, Version Version) frameworkVersion)
-        public static bool GetGeneratePackageOnBuild(string projectFileName)
-        public static string GetAssemblyName(string projectFileName)
-        public static string GetPackageId(string projectFileName)
-        public static List<(string PackageId, Version Version)> GetInstalledPackages(string projectFileName)
-        public static string GetInstalledPackageVersion(string projectFileName, string packageId)
-        public static bool UpdateInstalledPackageVersion(string projectFileName, string packageId, string version)
-        public static string GetVersion(string projectFileName)
-        public static string GetMaxVersionOfSolution(string solutionPath)
-        public static bool SetVersionToSolution(string solutionPath, string targetVersion)
-        public static bool SetVersion(string projectFileName, string targetVersion)
-        public static bool SetCopyright(string projectFileName, string copyright)
-        public static string GetOutputPath(string projectFileName, KindOfConfiguration kindOfConfiguration = KindOfConfiguration.Debug)
-        public static string GetDocumentationFile(string projectFileName, KindOfConfiguration kindOfConfiguration = KindOfConfiguration.Debug)
-        public static bool SetDocumentationFileInUnityProjectIfEmpty(string projectFileName, KindOfConfiguration kindOfConfiguration = KindOfConfiguration.Debug)
-        public static bool SetDocumentationFileIfEmpty(string projectFileName, string documentationFileName, KindOfConfiguration kindOfConfiguration = KindOfConfiguration.Debug)
-        public static bool SetDocumentationFile(string projectFileName, string documentationFileName, KindOfConfiguration kindOfConfiguration = KindOfConfiguration.Debug)
- */
