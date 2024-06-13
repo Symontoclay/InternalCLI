47be713dacb257aa3b82d3eb7669e5a3276210df
metatypeman <metatypeman@gmail.com>
2024-06-13 20:59:10 +0300

    0.5.4 UpdateTargetFrameworkInAllCSharpProjectsCommandLineParserTest

--- CLI.CmdlParser.Tests/UpdateTargetFrameworkInAllCSharpProjectsCommandLineParserTest.cs
+++ CLI.CmdlParser.Tests/UpdateTargetFrameworkInAllCSharpProjectsCommandLineParserTest.cs
@@ -1,11 +1,65 @@
-﻿namespace CLI.CmdlParser.Tests
+﻿using CSharpUtils;
+using SymOntoClay.CLI.Helpers.CommandLineParsing.Exceptions;
+using UpdateTargetFrameworkInAllCSharpProjects;
+
+namespace CLI.CmdlParser.Tests
 {
     public class UpdateTargetFrameworkInAllCSharpProjectsCommandLineParserTest
     {
         [Test]
         public void ValidCommandLine_Success()
         {
-            throw new NotImplementedException();
+            var args = new List<string>()
+            {
+                "NetStandard",
+                "2.0"
+            };
+
+            var parser = new UpdateTargetFrameworkInAllCSharpProjectsCommandLineParser(true);
+
+            var result = parser.Parse(args.ToArray());
+
+            Assert.That(result.Errors.Count, Is.EqualTo(0));
+
+            Assert.That(result.Params.Count, Is.EqualTo(2));
+
+            var targetFramework = (KindOfTargetCSharpFramework)result.Params["TargetFramework"];
+
+            Assert.That(targetFramework, Is.EqualTo(KindOfTargetCSharpFramework.NetStandard));
+
+            var targetVersion = (Version)result.Params["TargetVersion"];
+
+            Assert.That(targetVersion, Is.EqualTo(new Version(2, 0)));
+        }
+
+        [Test]
+        public void EmptyCommandLine_Fail()
+        {
+            var args = new List<string>();
+
+            var exception = Assert.Catch<RequiredOptionException>(() => {
+                var parser = new UpdateTargetFrameworkInAllCSharpProjectsCommandLineParser(false);
+                parser.Parse(args.ToArray());
+            });
+
+            Assert.That(exception.Message, Is.EqualTo("Required command line argument 'TargetFramework' must be entered."));
+        }
+
+        [Test]
+        public void EmptyCommandLine_ErrorsList()
+        {
+            var args = new List<string>();
+
+            var parser = new UpdateTargetFrameworkInAllCSharpProjectsCommandLineParser(true);
+
+            var result = parser.Parse(args.ToArray());
+
+            Assert.That(result.Errors.Count, Is.EqualTo(2));
+
+            Assert.That(result.Errors[0], Is.EqualTo("Required command line argument 'TargetFramework' must be entered."));
+            Assert.That(result.Errors[1], Is.EqualTo("Required command line argument 'TargetVersion' must be entered."));
+
+            Assert.That(result.Params.Count, Is.EqualTo(0));
         }
     }
 }
--- TestSandBox/TstUpdateTargetFrameworkInAllCSharpProjectsCmdlParsersHandler.cs
+++ TestSandBox/TstUpdateTargetFrameworkInAllCSharpProjectsCmdlParsersHandler.cs
@@ -14,10 +14,198 @@ namespace TestSandBox
         {
             _logger.Info("Begin");
 
-            UpdateTargetFrameworkInAllCSharpProjectsCase1();
+            //UpdateTargetFrameworkInAllCSharpProjectsCase5_1();//It should be tested.
+            //UpdateTargetFrameworkInAllCSharpProjectsCase5();//It should be tested.
+            //UpdateTargetFrameworkInAllCSharpProjectsCase4_1();//It should be tested.
+            //UpdateTargetFrameworkInAllCSharpProjectsCase4();//It should be tested.
+            //UpdateTargetFrameworkInAllCSharpProjectsCase3_1();//It should be tested.
+            //UpdateTargetFrameworkInAllCSharpProjectsCase3();//It should be tested.
             //UpdateTargetFrameworkInAllCSharpProjectsCase2();
+            UpdateTargetFrameworkInAllCSharpProjectsCase1_1();//It should be tested.
+            //UpdateTargetFrameworkInAllCSharpProjectsCase1();
 
             _logger.Info("End");
+        }
+
+        private void UpdateTargetFrameworkInAllCSharpProjectsCase5_1()//It should be tested.
+        {
+            try
+            {
+                var args = new List<string>()
+                {
+                    "TargetFramework",
+                    "NetStandard",
+                    "TargetVersion",
+                    "Cat"
+                };
+
+                var parser = new UpdateTargetFrameworkInAllCSharpProjectsCommandLineParser(false);
+
+                var result = parser.Parse(args.ToArray());
+
+                _logger.Info($"result = {result}");
+            }
+            catch (Exception e)
+            {
+                _logger.Info(e);
+            }
+        }
+
+        private void UpdateTargetFrameworkInAllCSharpProjectsCase5()//It should be tested.
+        {
+            try
+            {
+                var args = new List<string>()
+                {
+                    "NetStandard",
+                    "Cat"
+                };
+
+                var parser = new UpdateTargetFrameworkInAllCSharpProjectsCommandLineParser(false);
+
+                var result = parser.Parse(args.ToArray());
+
+                _logger.Info($"result = {result}");
+            }
+            catch (Exception e)
+            {
+                _logger.Info(e);
+            }
+        }
+
+        private void UpdateTargetFrameworkInAllCSharpProjectsCase4_1()//It should be tested.
+        {
+            try
+            {
+                var args = new List<string>()
+                {
+                    "TargetFramework",
+                    "NetStandard",
+                    "TargetVersion",
+                    "Cat"
+                };
+
+                var parser = new UpdateTargetFrameworkInAllCSharpProjectsCommandLineParser(false);
+
+                var result = parser.Parse(args.ToArray());
+
+                _logger.Info($"result = {result}");
+            }
+            catch (Exception e)
+            {
+                _logger.Info(e);
+            }
+        }
+
+        private void UpdateTargetFrameworkInAllCSharpProjectsCase4()//It should be tested.
+        {
+            try
+            {
+                var args = new List<string>()
+                {
+                    "NetStandard",
+                    "Cat"
+                };
+
+                var parser = new UpdateTargetFrameworkInAllCSharpProjectsCommandLineParser(false);
+
+                var result = parser.Parse(args.ToArray());
+
+                _logger.Info($"result = {result}");
+            }
+            catch (Exception e)
+            {
+                _logger.Info(e);
+            }
+        }
+
+        private void UpdateTargetFrameworkInAllCSharpProjectsCase3_1()//It should be tested.
+        {
+            try
+            {
+                var args = new List<string>()
+                {
+                    "TargetFramework",
+                    "CatStandard",
+                    "TargetVersion",
+                    "2.0"
+                };
+
+                var parser = new UpdateTargetFrameworkInAllCSharpProjectsCommandLineParser(false);
+
+                var result = parser.Parse(args.ToArray());
+
+                _logger.Info($"result = {result}");
+            }
+            catch (Exception e)
+            {
+                _logger.Info(e);
+            }
+        }
+
+        private void UpdateTargetFrameworkInAllCSharpProjectsCase3()//It should be tested.
+        {
+            try
+            {
+                var args = new List<string>()
+                {
+                    "CatStandard",
+                    "2.0"
+                };
+
+                var parser = new UpdateTargetFrameworkInAllCSharpProjectsCommandLineParser(false);
+
+                var result = parser.Parse(args.ToArray());
+
+                _logger.Info($"result = {result}");
+            }
+            catch (Exception e)
+            {
+                _logger.Info(e);
+            }
+        }
+
+        private void UpdateTargetFrameworkInAllCSharpProjectsCase2()
+        {
+            try
+            {
+                var args = new List<string>();
+
+                var parser = new UpdateTargetFrameworkInAllCSharpProjectsCommandLineParser(false);
+
+                var result = parser.Parse(args.ToArray());
+
+                _logger.Info($"result = {result}");
+            }
+            catch (Exception e)
+            {
+                _logger.Info(e);
+            }
+        }
+
+        private void UpdateTargetFrameworkInAllCSharpProjectsCase1_1()//It should be tested.
+        {
+            var args = new List<string>()
+            {
+                "TargetFramework",
+                "NetStandard",
+                "TargetVersion",
+                "2.0"
+            };
+
+            var parser = new UpdateTargetFrameworkInAllCSharpProjectsCommandLineParser(true);
+
+            var result = parser.Parse(args.ToArray());
+
+            _logger.Info($"result = {result}");
+
+            var targetFramework = (KindOfTargetCSharpFramework)result.Params["TargetFramework"];
+
+            _logger.Info($"targetFramework = {targetFramework}");
+
+            var targetVersion = (Version)result.Params["TargetVersion"];
+
+            _logger.Info($"targetVersion = {targetVersion}");
         }
 
         private void UpdateTargetFrameworkInAllCSharpProjectsCase1()
@@ -41,24 +229,6 @@ namespace TestSandBox
             var targetVersion = (Version)result.Params["TargetVersion"];
 
             _logger.Info($"targetVersion = {targetVersion}");
-        }
-
-        private void UpdateTargetFrameworkInAllCSharpProjectsCase2()
-        {
-            try
-            {
-                var args = new List<string>();
-
-                var parser = new UpdateTargetFrameworkInAllCSharpProjectsCommandLineParser(true);
-
-                var result = parser.Parse(args.ToArray());
-
-                _logger.Info($"result = {result}");
-            }
-            catch (Exception e)
-            {
-                _logger.Info(e);
-            }
         }
     }
 }
