79dde206cfbe53a28c0556ff19250ef2c27e883b
metatypeman <metatypeman@gmail.com>
2024-06-15 12:20:16 +0300

    0.5.4 UpdateTargetFrameworkInAllCSharpProjects

--- UpdateTargetFrameworkInAllCSharpProjects/Program.cs
+++ UpdateTargetFrameworkInAllCSharpProjects/Program.cs
@@ -4,6 +4,7 @@ using Deployment.DevTasks.TargetFrameworks.CheckTargetFrameworksInAllCSharpProje
 using Deployment.DevTasks.TargetFrameworks.UpdateTargetFrameworkInAllCSharpProjects;
 using Newtonsoft.Json;
 using NLog;
+using SymOntoClay.CLI.Helpers;
 
 namespace UpdateTargetFrameworkInAllCSharpProjects
 {
@@ -15,102 +16,81 @@ namespace UpdateTargetFrameworkInAllCSharpProjects
         {
             AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 
-            if(args.Length == 0)
-            {
-                Console.WriteLine("You shoud input kind of target framework and target version in command line.");
-                Console.WriteLine("The first argument should be name of target framework, next target version after space.");
-                Console.WriteLine("For example: 'Net 8.0'");
-                Console.WriteLine("The kind of target framework is an option of enum KindOfTargetCSharpFramework.");
-                Console.WriteLine("The target version should only contain digits and point, for example: '8.0', '3.1.2'");
-                Console.WriteLine("Press any key for exit");
-
-                Console.ReadKey();
-
-                return;
-            }
-
-            if(args.Length < 2)
-            {
-                Console.WriteLine("Not enough arguments! So target version argument is absent.");
-                Console.WriteLine("Press any key for exit");
-
-                Console.ReadKey();
-
-                return;
-            }
-
-            if(args.Length > 2)
-            {
-                Console.WriteLine("Too many arguments!");
-                Console.WriteLine("Press any key for exit");
-
-                Console.ReadKey();
-
-                return;
-            }
-
 #if DEBUG
             //_logger.Info($"args = {JsonConvert.SerializeObject(args, Formatting.Indented)}");
 #endif
 
-            var kindOfTargetCSharpFrameworkStr = args[0];
-            var targetVersionStr = args[1];
+            ConsoleWrapper.WriteCopyright();
 
-#if DEBUG
-            //_logger.Info($"kindOfTargetCSharpFrameworkStr = {kindOfTargetCSharpFrameworkStr}");
-            //_logger.Info($"targetVersionStr = {targetVersionStr}");
-#endif
+            var parser = new UpdateTargetFrameworkInAllCSharpProjectsCommandLineParser(true);
 
-            if(!Enum.TryParse<KindOfTargetCSharpFramework>(kindOfTargetCSharpFrameworkStr, out var kindOfTargetCSharpFramework))
+            var result = parser.Parse(args.ToArray());
+
+            if(result.Errors.Count > 1)
             {
-                Console.WriteLine($"Unknown target framework '{kindOfTargetCSharpFrameworkStr}'");
-                Console.WriteLine("Press any key for exit");
+                foreach(var error in result.Errors)
+                {
+                    ConsoleWrapper.WriteError(error);
+                }
+
+                PrintHelp();
+
+                ConsoleWrapper.WriteText("Press any key for exit");
 
                 Console.ReadKey();
 
                 return;
             }
 
-#if DEBUG
-            //_logger.Info($"kindOfTargetCSharpFramework = {kindOfTargetCSharpFramework}");
-#endif
-
-            if(!Version.TryParse(targetVersionStr, out var targetVersion))
+            if (result.Params.Count == 0)
             {
-                Console.WriteLine($"Bad version format '{targetVersionStr}'");
-                Console.WriteLine("Press any key for exit");
+                PrintHelp();
+
+                ConsoleWrapper.WriteText("Press any key for exit");
 
                 Console.ReadKey();
 
                 return;
             }
 
-            Console.WriteLine($"Version of all C# projects with target framework {kindOfTargetCSharpFramework} will be changed to {targetVersionStr}.");
-            Console.WriteLine("Are you sure?");
-            Console.WriteLine("Press 'y' or 'Y' for continue or other else key for cancel.");
-            Console.WriteLine("After your choise press enter.");
+            var kindOfTargetCSharpFramework = (KindOfTargetCSharpFramework)result.Params["TargetFramework"];
+            var targetVersion = (Version)result.Params["TargetVersion"];
+
+            ConsoleWrapper.WriteText($"Version of all C# projects with target framework {kindOfTargetCSharpFramework} will be changed to {targetVersion}.");
+            ConsoleWrapper.WriteText("Are you sure?");
+            ConsoleWrapper.WriteText("Press 'y' or 'Y' for continue or other else key for cancel.");
+            ConsoleWrapper.WriteText("After your choise press enter.");
 
             var key = Console.ReadLine();
 
             if (key != "y" && key != "Y")
             {
-                Console.WriteLine("Updating has been cancelled.");
+                ConsoleWrapper.WriteText("Updating has been cancelled.");
 
                 return;
             }
 
-            Console.WriteLine("Updating has been started.");
+            ConsoleWrapper.WriteText("Updating has been started.");
 
             var deploymentPipeline = new DeploymentPipeline();
 
             deploymentPipeline.Add(new UpdateTargetFrameworkInAllCSharpProjectsDevTask(new UpdateTargetFrameworkInAllCSharpProjectsDevTaskOptions()
             {
                 KindOfTargetCSharpFramework = kindOfTargetCSharpFramework,
-                Version = targetVersionStr
+                Version = targetVersion.ToString()
             }));
             deploymentPipeline.Add(new CheckTargetFrameworksInAllCSharpProjectsDevTask());
 
             deploymentPipeline.Run();
+        }
+
+        private static void PrintHelp()
+        {
+            Console.WriteLine("You shoud input kind of target framework and target version in command line.");
+            Console.WriteLine("The first argument should be name of target framework, next target version after space.");
+            Console.WriteLine("For example: 'Net 8.0'");
+            Console.WriteLine("The kind of target framework is an option of enum KindOfTargetCSharpFramework.");
+            Console.WriteLine("The target version should only contain digits and point, for example: '8.0', '3.1.2'");
         }
 
         private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
