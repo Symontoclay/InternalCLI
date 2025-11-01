f61b79bc890f36b9b859dd8a0b1e9845031de4c8
metatypeman <metatypeman@gmail.com>
2025-11-01 18:24:35 +0200

    Added ProdSiteBuild

---
+++ ProdSiteBuild/Program.cs
@@ -0,0 +1,23 @@
+﻿using CommonUtils.DeploymentTasks;
+using Deployment.ReleaseTasks.ProdSiteBuild;
+using NLog;
+
+namespace ProdSiteBuild
+{
+    internal class Program
+    {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            DeploymentPipeline.Run(new ProdSiteBuildReleaseTask());
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            _logger.Info($"e.ExceptionObject = {e.ExceptionObject}");
+        }
+    }
+}
