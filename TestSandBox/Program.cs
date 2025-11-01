ba3d83b7b1cf3c46c96e668a74428c858c5e8213
metatypeman <metatypeman@gmail.com>
2025-11-01 12:47:00 +0200

    snapshot

--- TestSandBox/Program.cs
+++ TestSandBox/Program.cs
@@ -140,7 +140,7 @@ namespace TestSandBox
                 //TstSetXmlDocFileNameToCsProj();
                 //TstRemoveDir();
                 //TstFinishRelease0_3_6_p();
-                //TstFinishRelease0_3_6();//<--- It has been used when 0.4.0 release has been filed.
+                TstFinishRelease0_3_6();//<--- It has been used when 0.4.0 release has been filed.
                 //TstFinishRelease0_3_2();
                 //TstRestoreSlnInUnityProject();
                 //TstTesting();
@@ -163,7 +163,7 @@ namespace TestSandBox
                 //TstSiteSettings();
                 //TstFutureReleaseInfo();
                 //TstFutureReleaseInfoSource();
-                TstProjectsDataSource();
+                //TstProjectsDataSource();
                 //TstGetEnvironmentVariables();
                 //TstReleaseItemsHandler();
                 //TstLessHandler();
